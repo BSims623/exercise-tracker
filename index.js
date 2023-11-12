b0f58bf6ba24f32122b2f6a98a9b1b77223d2651
Brian Sims <briansims623@gmail.com>
2023-11-11 22:15:59 -0800

    fix: URI to MONGO_URI

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const port = process.env.PORT || 3000
 
 const start = async () => {
   try {
-    connectDB(process.env.URI)
+    connectDB(process.env.MONGO_URI)
     app.listen(port, () => {
       console.log(`Server is listening on port ${port}...`)
     })
