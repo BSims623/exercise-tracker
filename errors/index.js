53fe1175c478462f0da7df56cdd61c0b636aff25
Brian Sims <briansims623@gmail.com>
2023-11-13 14:26:10 -0800

    fix: refactor

--- errors/index.js
+++ errors/index.js
@@ -1,13 +1,11 @@
 const CustomApiError = require('./custom-error')
 const ProvideInputError = require('./provide-input')
-const UsernameAlreadyExistsError = require('./username-already-exists')
 const UserNotFoundError = require('./not-found')
 const InvalidError = require('./invalid-entry')
 
 module.exports = {
     CustomApiError,
     ProvideInputError,
-    UsernameAlreadyExistsError,
     UserNotFoundError,
     InvalidError
 }
--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,5 @@ router.route('/').post(createNewUser)
 router.route('/').get(getAllUsers)
 router.route('/:_id/exercises').post(addNewExercise)
 router.route('/:_id/logs').get(getUserLogs)
-//router.route('/:_id/logs?[from][&to][&limit]', getUserExerciseLog)
 
 module.exports = router
